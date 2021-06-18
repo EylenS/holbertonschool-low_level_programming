fa61130f2c93c55906c71cd9ef1650b978229b6f
EylenS <3359@holbertonschool.com>
2021-06-18 12:00:59 +0700

    SiZe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,7 +20,7 @@ a = 'h';
 b = 8;
 c = 7;
 d = 3456;
-e = 34,6;
+e = 34.6;
 printf("Size of a char: %d byte(s)\n", sizeof(a));
 printf("Size of an int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %d byte(s)\n", sizeof(c));
