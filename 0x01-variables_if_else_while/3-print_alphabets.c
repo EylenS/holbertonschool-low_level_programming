8b9b46f2381c6a24176217edcf90823d825df7e6
EylenS <3359@holbertonschool.com>
2021-06-20 03:25:44 +0700

    lower and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - breve descripcion
+ * Description: use print hello world
+ * Return: void
+*/
+int main(void)
+{
+	char letra;
+
+	for (letra = 97; letra <= 122; letra++)
+		putchar(letra);
+	for (letra = 65; letra <= 90; letra++)
+		putchar(letra);
+	putchar('\n');
+return (0);
+}
+
