9909d649b5233f3b9453695415d96b2f4e39fd60
EylenS <3359@holbertonschool.com>
2021-06-20 04:58:13 +0700

    putchar,!= 2 letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - breve descripcion
+ * Description: use print hello world
+ * Return: void
+*/
+int main(void)
+{
+char letra;
+
+	for (letra = 97; letra <= 122; letra++)
+		if ((letra != 101) && (letra != 113))
+		   putchar(letra);
+    putchar('\n');
+return (0);
+}
+
