69e578ffae9a32587fbc46520f620ec4caedf58a
EylenS <3359@holbertonschool.com>
2021-06-20 10:39:40 +0700

    + numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,16 @@ int main(void)
 {
 	int a;
 
-	for (a = 48; a < 57; a++)
+	for (a = 48; a < 58; a++)
 	{
 		putchar(a);
-		putchar(',');
-		putchar (' ');
+		if (a < 57)
+		{
+			putchar(',');
+			putchar (' ');
+		}
 	}
-	putchar(57);
+	putchar('\n');
 	return (0);
 }
 
