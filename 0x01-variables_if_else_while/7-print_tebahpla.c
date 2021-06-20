cd94cc6d117b7c5e6495f52cff72442232bee5ca
EylenS <3359@holbertonschool.com>
2021-06-20 09:27:16 +0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - breve description
+ * Description: use print hello world
+ * Return: void
+*/
+int main(void)
+{
+	char l;
+
+	for (l = 122; l >= 97; l--)
+		putchar(l);
+putchar('\n');
+return (0);
+}
+
