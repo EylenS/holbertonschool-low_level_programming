2262252dadcd96ac934bda8b06b527876655766f
EylenS <3359@holbertonschool.com>
2021-06-22 08:50:48 +0700

    Lower or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _islower - writes the character is in lowercase to stdout
+ * : The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
