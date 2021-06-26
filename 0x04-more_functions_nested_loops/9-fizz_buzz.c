3cab35e8b5b85c3d06a9746e792ceec1cd0a1315
EylenS <3359@holbertonschool.com>
2021-06-27 05:12:51 +0700

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d", i);
+		printf(" ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
