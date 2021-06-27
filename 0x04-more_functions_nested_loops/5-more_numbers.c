eb6bbd2f2fe596aa41eddc73fc9b88554629ea68
EylenS <3359@holbertonschool.com>
2021-06-27 08:55:21 +0700

    10x No

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * more_numbers - prints 10 times the numbers (0-14)
+ * more_numbers - writes the character c to stdout
  *
  *
  * Return: On success 1.
@@ -12,13 +12,13 @@ void more_numbers(void)
 	int i, j;
 
 	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
 		{
-			for (j = 0; j <= 14; j++)
-				{
-					if (j >= 10)
-						_putchar(j / 10 + '0');
-					_putchar(j % 10 + '0');
-				}
-			_putchar('\n');
+			if (j >= 10)
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 		}
+		_putchar('\n');
+	}
 }
