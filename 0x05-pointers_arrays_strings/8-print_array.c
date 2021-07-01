3c6895017340615a29fdf8f286e2122a94898c21
EylenS <3359@holbertonschool.com>
2021-07-01 09:35:22 +0700

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,12 +13,13 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= (n - 1); i++)
-	{
-		printf("%i", *(a + i));
-		if (i != n - 1)
-			printf(", ");
-		else
-			printf("\n");
-	}
+	if (n >= 0)
+		for (i = 0; i <= (n - 1); i++)
+		{
+			printf("%i", *(a + i));
+			if (i != n - 1)
+				printf(", ");
+			else
+				printf("\n");
+		}
 }
