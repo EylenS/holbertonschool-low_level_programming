fbb2094589657514f8b8e3be4dba3343fd99e708
EylenS <3359@holbertonschool.com>
2021-07-01 09:39:58 +0700

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+ #include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -13,7 +13,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n >= 0)
+	if (n > 0)
 		for (i = 0; i <= (n - 1); i++)
 		{
 			printf("%i", *(a + i));
@@ -22,4 +22,6 @@ void print_array(int *a, int n)
 			else
 				printf("\n");
 		}
+	else
+		printf("\n");
 }
