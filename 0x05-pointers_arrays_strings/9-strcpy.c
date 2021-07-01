f71dc2e60361311236c754350ab7206e2a937514
EylenS <3359@holbertonschool.com>
2021-07-01 10:08:37 +0700

    _strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+ #include "holberton.h"
+
+/**
+ * _strcpy - Copies the string pointed to another one
+ * @dest: pointer destiny
+ * @src: pointer source
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
