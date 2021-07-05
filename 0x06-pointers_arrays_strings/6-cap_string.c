3bddd58f71604824f13b058d8fd1b2d37e47aeae
EylenS <3359@holbertonschool.com>
2021-07-05 07:45:49 +0700

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,10 +15,18 @@ char *cap_string(char *str)
 	{
 		if (str[i] >= 97 && str[i] <= 122)
 		{
-			if (str[i - 1] == 9 || str[i - 1] == ' ' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || str[i - 1] == '\n')
-				str[i] = str[i] -32;
+			if (str[i - 1] == 9 || str[i - 1] == ' ' || str[i - 1] == ',')
+				str[i] = str[i] - 32;
+			if (str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!')
+				str[i] = str[i] - 32;
+			if (str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(')
+				str[i] = str[i] - 32;
+			if (str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+				str[i] = str[i] - 32;
+			if (str[i - 1] == '\n')
+				str[i] = str[i] - 32;
 		}
 		i++;
 	}
 	return (str);
-}			
+}
