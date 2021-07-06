e0cbd3a51fe140c7d9f1127f926daae778a47726
EylenS <3359@holbertonschool.com>
2021-07-07 03:22:40 +0700

    _strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk- Finds the 1st char in the string s that matches any char specified in accept
+ * @s:This is the string to be scanned
+ * @accept:The string containing the list of chars to match
+ *
+ * Return: a pointer to the char in s that matches one of the chars in accept
+ * NULL if no such char is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+			j++;
+		}
+		i++;
+	}
+	return (00);
+}
