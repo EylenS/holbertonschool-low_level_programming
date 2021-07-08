29efab1db4c4c05ba765ee90d5786feb5b876c1d
EylenS <3359@holbertonschool.com>
2021-07-09 05:39:54 +0700

    _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion -Takes 2 arguments and, returns the power raised 2 the base
+ * @x: Base value
+ * @y: number of times x is multiplied by itself
+ *
+ * Return: return the factorial of a given number
+ * On error: -1
+ */
+int _pow_recursion(int x, int y)
+{
+	int rise_x = 0;
+
+	if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	else if (y > 1)
+	{
+		rise_x = _pow_recursion(x, (y - 1)) * x;
+		return (rise_x);
+	}
+	else
+		return (-1);
+}
