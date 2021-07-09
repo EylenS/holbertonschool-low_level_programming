2d8727efd6671b6da764016b463ca53fd0e2c1da
EylenS <3359@holbertonschool.com>
2021-07-10 06:49:57 +0700

    argc & argv unused

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,8 +9,8 @@
  * Return: On success 0.
  * On error, other
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
