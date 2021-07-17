924f4c9524859fb236465662efe985e46465d0ce
EylenS <3359@holbertonschool.com>
2021-07-17 08:54:14 +0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,20 +15,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, size, size_s1, size_s2;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
-		return (00);
-	i = 0;
-	while (s1[i] != '\0')
+	if (s1 != NULL)
 	{
-		i++;
+		i = 0;
+		while (s1[i] != '\0')
+			i++;
+		size_s1 = i;
 	}
-	size_s1 = i;
-	i = 0;
-	while (s2[i] != '\0')
+	else
+		size_s1 = 0;
+	if (s2 != NULL)
 	{
-		i++;
+		i = 0;
+		while (s2[i] != '\0')
+			i++;
+		size_s2 = i;
 	}
-	size_s2 = i;
+	else
+		size_s2 = 0;
 	if (n < size_s2)
 		size = size_s1 + n + 1;
 	else
