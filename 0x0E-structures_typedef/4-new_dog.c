a4237921203488a367c9c09c1f64f30f5d5ecbe1
EylenS <3359@holbertonschool.com>
2021-07-21 09:42:24 +0700

    creating a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates new information
+ * @name: pet's name
+ * @age: pet's age
+ * @owner: owner's name
+ *
+ * Return: pointer.
+ * On error, -1
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		exit(-1);
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - Short information about a pet
  * @name: dog's name
  * @age: dog's age
  * @owner: name of the owner
  *
- * Description: Longer description
+ * Description: information about pets
  */
 struct dog
 {
@@ -22,4 +24,5 @@ typedef struct
 	float age;
 	char *owner;
 } dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
