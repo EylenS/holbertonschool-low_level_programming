e8558a9fb35333ecaf5f376b4008f4e66fe1f894
EylenS <3359@holbertonschool.com>
2021-07-22 08:30:21 +0700

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - This function prints a name
+ * @name: name of the person
+ * @f: function pointer
+ *
+ * Return: void function
+ * On error, exit
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
