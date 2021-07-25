e2054a4e29a2bd16bf9e945a10621eb816a02251
EylenS <3359@holbertonschool.com>
2021-07-25 08:04:16 +0700

    print_all adjusted

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,8 +2,65 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
+int pr_char(va_list list);
+int pr_int(va_list list);
+int pr_float(va_list list);
+int pr_string(va_list list);
+
 /**
- * print_all - Prints anything
+ * pr_char - Prints the argument type char
+ * @list: argument
+ *
+ * Return: On success 0
+ * Take into account the data structure
+ */
+int pr_char(va_list list)
+{
+	printf("%c", va_arg(list, int));
+	return (0);
+}
+
+/**
+ * pr_int - Prints the argument type int
+ * @list: argument
+ *
+ * Return: On success 0
+ * Take into account the data structure
+ */
+int pr_int(va_list list)
+{
+	printf("%d", va_arg(list, int));
+	return (0);
+}
+
+/**
+ * pr_float - Prints argument type float
+ * @list: argument
+ *
+ * Return: On success 0
+ * Take into account the data structure
+ */
+int pr_float(va_list list)
+{
+	printf("%f", va_arg(list, double));
+	return (0);
+}
+
+/**
+ * pr_string - Prints argument type string
+ * @list: argument
+ *
+ * Return: On success 0
+ * Take into account the data structure
+ */
+int pr_string(va_list list)
+{
+	printf("%s", va_arg(list, char *));
+	return (0);
+}
+
+/**
+ * print_all - Prints anything type data
  * @format: list of types of arguments passed to the function
  * c: chart, i: int, f: float, s: char *
  *
@@ -12,34 +69,35 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	char *buffer;
-	int i = 0;
+	op_t ops[4] = {
+		{'c', pr_char},
+		{'i', pr_int},
+		{'f', pr_float},
+		{'s', pr_string}
+};
 
-	va_start(args, format);
-	while (*(format + i) != '\0')
+va_list args;
+int i, j;
+i = 0;
+j = 0;
+
+va_start(args, format);
+
+while (format[i] != '\0')
+{
+	j = 0;
+	while (j < 4)
 	{
-		switch (*(format + i))
+		if (format[i] == ops[j].pr)
 		{
-		case 'c':
-			printf("%c", va_arg(args, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(args, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(args, double));
-			break;
-		case 's':
-			buffer = va_arg(args, char *);
-			if (buffer != NULL)
-				printf("%s", buffer);
-			else
-				printf("(nil)");
-			break;
+			ops[j].f(args);
+			if (format[i + 1] != '\0')
+				printf(", ");
 		}
-		i++;
+		j++;
 	}
-	va_end(args);
-	printf("\n");
+	i++;
+}
+va_end(args);
+printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -11,5 +11,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int pr_char(va_list list);
+int pr_int(va_list list);
+int pr_float(va_list list);
+int pr_string(va_list list);
+
+/*structs*/
+typedef struct op
+{
+	char pr;
+	int (*f)(va_list);
+} op_t;
 
 #endif
