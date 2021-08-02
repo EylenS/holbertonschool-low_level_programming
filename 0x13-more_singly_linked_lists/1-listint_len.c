c4145d5b725fb7980c9731201faac4a1d2170368
eylens <eymasac@gmail.com>
2021-08-02 18:17:36 -0500

    print list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * listint_len - Prints all the elements of a listint_t list
+ * @h:The pointer
+ *
+ * Return: the number of nodes. On Error return negative number.
+ */
+size_t listint_len(const listint_t *h)
+{
+
+}
