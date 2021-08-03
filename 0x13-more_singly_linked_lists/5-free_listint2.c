42493f488cb777dd34cc8270bada304a3f12543d
eylens <eymasac@gmail.com>
2021-08-03 18:00:15 -0500

    free with pointer to pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2- free the memory.
+ * @head: The pointer.
+ *
+ * The function sets the head to NULL
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *p;
+p = *head;
+while (p != NULL)
+{
+p = p->next;
+free(*head);
+*head = p;
+}
+}
