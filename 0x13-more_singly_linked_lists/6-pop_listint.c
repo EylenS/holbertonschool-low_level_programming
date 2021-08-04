9bf552e990e256fc60a1957688762b24a96d9126
eylens <eymasac@gmail.com>
2021-08-04 13:32:26 -0500

    change in if

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,11 +11,14 @@ int pop_listint(listint_t **head)
 	listint_t *p;
 	int data_n;
 
-	if (*head == NULL)
+	if (*head != NULL)
+	{
+		p = *head;
+		*head = p->next; /* head apuntará al siguiente nodo*/
+		data_n = p->n;
+		free(p); /* head está apdo al sig nodo, y p borraría el head node */
+		return (data_n);
+	}
+	else
 		return (0);
-	p = *head;
-	*head = p->next; /* head apuntará al siguiente nodo*/
-	data_n = p->n;
-	free(p); /* head está apdo al sig nodo, y p borraría el head node */
-	return (data_n);
 }
