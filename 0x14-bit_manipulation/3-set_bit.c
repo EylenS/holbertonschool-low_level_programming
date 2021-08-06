4a3a34467ebf696ac38fbae9c2da52819280d327
eylens <eymasac@gmail.com>
2021-08-05 23:23:37 -0500

    sets val of a bit on idx

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,12 +9,12 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    unsigned int nu_bit = sizeof(*n) * 8; /* number of bits */
+	unsigned int nu_bit = sizeof(*n) * 8; /* number of bits */
 	unsigned int i;
 
 	if (index > nu_bit)
 		return (-1);
 	i = 1 << index; /*move the 1st bite on 1 to the left 2 change*/
-    *n = *n | i; /* the new numb n, after change the bit in idx*/
-        return (1);
+	*n = *n | i;    /* the new numb n, after change the bit in idx*/
+	return (1);
 }
