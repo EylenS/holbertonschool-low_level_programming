7e7bf8c17648a7d9ab3a0df08aca5b589a44870d
eylens <eymasac@gmail.com>
2021-08-05 21:02:34 -0500

    binary operator &

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_binary - prints lon integer as binary number.
+ * @n: The long int to convert in binary number.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	if ((n & 1) == 1)
+		_putchar(49);
+	else
+		_putchar(48);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,6 @@
 int _putchar(char c);
 int _pow(int x, int y);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
