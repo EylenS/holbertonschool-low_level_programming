ff8bc12475376e638a4fc4e4efe5d93360294f49
eylens <eymasac@gmail.com>
2021-08-12 17:11:29 -0500

    2nd change

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,17 @@ int create_file(const char *filename, char *text_content)
 	if (filename != NULL)
 	{
 		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
-		if (fd != 3)
+		if (fd < 0)
 			return (-1);
-		while (*(text_content + i) != '\0') /* size of the string */
+		if (text_content != NULL)
 		{
+			while (*(text_content + i) != '\0') /* size of the string */
+			{
+				i++;
+			}
 			i++;
 		}
-		nbyte = write(fd, text_content, i + 1);
+		nbyte = write(fd, text_content, i);
 		if (nbyte < 0)
 			return (-1);
 		close(fd);
