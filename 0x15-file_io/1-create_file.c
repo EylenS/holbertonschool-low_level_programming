2bc58e21eb3db9ca17f095b12037ef8dd55e4bc1
eylens <eymasac@gmail.com>
2021-08-12 15:31:14 -0500

    create a file with 1st change

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename != NULL)
 	{
-		fd = open(filename, O_CREAT | O_RDWR | O_EXCL | O_TRUNC, S_IRUSR | S_IWUSR);
+		fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (fd != 3)
 			return (-1);
 		while (*(text_content + i) != '\0') /* size of the string */
