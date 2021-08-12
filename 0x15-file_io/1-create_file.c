5e268a95cb9427d4d33c2593bf4fbf4e9f26423e
eylens <eymasac@gmail.com>
2021-08-12 15:09:47 -0500

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - Reads a text file and prints it to the std output.
+ * @filename: Name of the file to create.
+ * @text_content: The string to be written in filename..
+ * Return: 1 on success. On failure, return (-1).
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i = 0;
+	ssize_t nbyte;
+
+	if (filename != NULL)
+	{
+		fd = open(filename, O_CREAT | O_RDWR | O_EXCL | O_TRUNC, S_IRUSR | S_IWUSR);
+		if (fd != 3)
+			return (-1);
+		while (*(text_content + i) != '\0') /* size of the string */
+		{
+			i++;
+		}
+		nbyte = write(fd, text_content, i + 1);
+		if (nbyte < 0)
+			return (-1);
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
