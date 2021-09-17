b0e2fec8bf269825e2730f237af6b803fac316e8
eylens <eymasac@gmail.com>
2021-09-17 12:40:08 -0500

    to get the in at the idx given

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Returns the nth node of a doubly linked list.
+ * @head: pointer to head of the current list
+ * @index: is the index of the node, starting from 0
+ * Return: the nth node of the list, if the node doesn't exist, NULL
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (counter < index)
+	{
+		counter++;
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
+}
