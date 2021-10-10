3aa12883b6f63c170ba12d67ed72371bdc1e2982
eylens <eymasac@gmail.com>
2021-10-10 17:07:35 -0500

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,24 +11,24 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new;
-    unsigned int idx;
+	hash_node_t *new;
+	unsigned int idx;
 
-    if (ht == NULL)
-        return (0);
-    if (key == NULL || strlen(key) == 0)
-        return(0);
-    
-    idx = key_index((unsigned char *)key, ht->size);
-    
-    new = malloc(sizeof(hash_node_t));
-    if (new == NULL)
-        return (0);
-    new->key = strdup(key);
-    new->value = strdup(value);
-    if (ht->array[idx] != NULL)
-        new->next = ht->array[idx];
-    new->next = NULL;
-    ht->array[idx] = new;
-    return (1);
+	if (ht == NULL)
+		return (0);
+	if (key == NULL || strlen(key) == 0)
+		return(0);
+	
+	idx = key_index((unsigned char *)key, ht->size);
+	
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (ht->array[idx] != NULL)
+		new->next = ht->array[idx];
+	new->next = NULL;
+	ht->array[idx] = new;
+	return (1);
 }
