b8902e404a087890dba9d584bae5d65da003c6a6
eylens <eymasac@gmail.com>
2021-10-10 17:09:37 -0500

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,8 +3,8 @@
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table to add or update the key/value to
- * @key:  is the key.
- * @value: is the value associated with the key. 
+ * @key: is the key
+ * @value: is the value associated with the key.
  * key can not be an empty string, while value can be an empty string
  * Return: 1 if it succeeded, 0 otherwise
  * In case of collision, add the new node at the beginning of the list
@@ -17,10 +17,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL)
 		return (0);
 	if (key == NULL || strlen(key) == 0)
-		return(0);
-	
+		return (0);
+
 	idx = key_index((unsigned char *)key, ht->size);
-	
+
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
