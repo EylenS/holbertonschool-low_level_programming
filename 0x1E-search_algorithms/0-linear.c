f6518685da4e66e820353f2a0244d4f447050691
eylens <eymasac@gmail.com>
2022-01-18 15:38:50 -0500

    trying other way to analyze a null array

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,14 +13,16 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t idx;
 
-    if (array == NULL)
-        return (-1);
-
-    for(idx = 0; idx < size; idx++)
+    if (array != NULL)
     {
-        printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
-        if (array[idx] == value)
-            return (idx);
+        for(idx = 0; idx < size; idx++)
+        {
+            printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
+            if (array[idx] == value)
+                return (idx);
+        }
+        return (-1);
     }
-    return (-1);
+    else
+        return (-1);
 }
