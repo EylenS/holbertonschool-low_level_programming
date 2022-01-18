11c2e40b05ba7ac34930b0995ecc93c30e80933d
eylens <eymasac@gmail.com>
2022-01-18 15:33:43 -0500

    include array null and it the value was not found it in different ways

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,14 +13,14 @@ int linear_search(int *array, size_t size, int value)
 {
     size_t idx;
 
-    if (array != NULL)
+    if (array == NULL)
+        return (-1);
+
+    for(idx = 0; idx < size; idx++)
     {
-        for(idx = 0; idx < size; idx++)
-        {
-            printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
-            if (array[idx] == value)
-                return (idx);
-        }
+        printf("Value checked array[%lu] = [%d]\n", idx, array[idx]);
+        if (array[idx] == value)
+            return (idx);
     }
     return (-1);
 }
